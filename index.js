453063b82df6c52b5449ef150d9cde538e3eafe3
saipabi <saipabi123@gmail.com>
2024-08-16 14:59:37 +0530

    responsive

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log("hi");
+// function fulname(fulname) {
+//     const result = fulname? fulname: "print no fulname"
+//     console.log("my full name is" + result)
+// };
+
+// fulname("suriya rajesh both of them got job");
+
+// const user = {"name": "rajesh", "age": 41}
+// console.log(user.name);
+
+// const greetings = (user)=>{
+//     const name = user? user.name : "nnil"
+
+//     return "Hello" + name
+// }
+// console.log(greetings(user));
+
+// const greeting = (user)=> {
+//     const name = pabi
+//     return user ? user: "nil"
+    
+
+// }
+
+// console.log(greeting());
+// age = 23;
+
+// const result = age>18?"eligible" : "not eligible"
+
+
+// console.log(result);
+
+const age = 18
+const results = age>21? "eligible": "not eligible"
+console.log(results);
+
+const avg = 75;
+const mark = avg >=90?"A grade": avg>=80? "B grade": "C grade";
+console.log(mark);
